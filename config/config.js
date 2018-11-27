a696bccb0256602e797670e87418d7dfabd80a0d
WallyThePenguin <mansourwaleed04@gmail.com>
2018-11-26 21:12:08 -0500

    nixx

---
+++ config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    settings:{
+        token: "NTE2MzkwNjI5NzMxODYwNTEw.Dty9_Q.lXbe9zhL2hmjiS-0EXKIjyntjhI",
+        botSupportTeam: [
+            "258337390643511306"
+        ],
+        password:"not-set-for-customer"
+    }
+}
